5dd430437adbd37dc7c69711b981c75b393773a8
Adriaan <102516695+adriaanlemstra@users.noreply.github.com>
2024-12-22 23:55:54 +0100

    Data Folder

--- Code/[5] combining_datasets.py
+++ Code/[5] combining_datasets.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import re
 
+# load the flavourDB and nevo nutritional info files
 flavour_DB_path = 'chefgpt/resources/flavourDB2_molecules.csv'
 nevo_path = 'chefgpt/resources/NEVO_Joined_cleaned.csv'
 
 flavour_df = pd.read_csv(flavour_DB_path)
 nevo_df = pd.read_csv(nevo_path, sep=';')
 
+# Clean and tokenize both nutritional and flavour datasets
 flavour_df['clean_ingredient'] = flavour_df['ingredient'].apply(
     lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', str(x).lower().strip()))
 nevo_df['clean_flavour_name'] = nevo_df['flavour_name'].apply(
     lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', str(x).lower().strip()))
 
+# Renaming columns and reformatting column types (ID was changed due to it causing errors. It was changed to a string)
 nevo_df.rename(columns={'flavour_index': 'ID'}, inplace=True)
 nevo_df['ID'] = nevo_df['ID'].dropna().astype(int).astype(str)
 
+# Distinguishijng between columns based on the type of data in said column. 
 non_numeric_columns = nevo_df.select_dtypes(exclude='number').columns
 numeric_columns = nevo_df.select_dtypes(include='number').columns
 
+
+# grouping data in rows (based on ingredient names), to reduce dimensionality. 
 nevo_df_grouped = (
     nevo_df.groupby(['clean_flavour_name', 'ID'], as_index=False)
     .agg({**{col: 'first' for col in non_numeric_columns}, **{col: 'mean' for col in numeric_columns}})
 )
 
+# Combine the dataframes in order to have a final dataset
 combined_df = pd.merge(
     flavour_df,
     nevo_df_grouped,
-    how='inner',  
-    left_on='clean_ingredient',
-    right_on='clean_flavour_name'
+    how = 'inner',  
+    left_on = 'clean_ingredient',
+    right_on = 'clean_flavour_name'
 )
 
-print(combined_df.columns)
+# print(combined_df.columns)
+
 
 molecules_column = combined_df.pop('molecule_list')
 combined_df['molecule_list'] = molecules_column
